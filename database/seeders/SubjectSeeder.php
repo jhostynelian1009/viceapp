f4e667931c676a301025d267a1ddd4854673d3ef
jhostynelian1009 <jhostynelianb@gmail.com>
2026-08-01 09:15:30 -0500

    Implementacion de materias

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subjects = [
+            'Matemáticas',
+            'Lengua y Literatura',
+            'Ciencias Naturales',
+            'Ciencias Sociales',
+            'Educación Física',
+            'Educación Cultural y Artística',
+            'Inglés',
+            'Informática'
+        ];
+
+        foreach ($subjects as $subject) {
+            \App\Models\Subject::firstOrCreate(['name' => $subject]);
+        }
+    }
+}
